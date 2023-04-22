104b370faed20a2c59fe463a51e2454721e2077e
Saurabh Singh <108921132+codergift@users.noreply.github.com>
2023-04-22 17:02:49 +0530

    Too many promises in life(task 12)

--- check.js
+++ check.js
@@ -1,2 +1,51 @@
-var headerTitle =document.getElementById('header-title');
-headerTitle.style.borderBottom='solid 3px #000';
+let posts = [];
+let lastActivityTime = null;
+
+function createPost(postData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(postData);
+      resolve();
+    }, 1000);
+  });
+}
+
+function updateLastUserActivityTime() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      lastActivityTime = new Date();
+      console.log("User's last activity time updated to:", lastActivityTime);
+      resolve();
+    }, 1000);
+  });
+}
+
+function deleteLastPost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length === 0) {
+        reject(new Error("No posts to delete"));
+      } else {
+        posts.pop();
+        console.log("Last post deleted");
+        resolve();
+      }
+    }, 1000);
+  });
+}
+
+// Usage:
+createPost({ title: 'First post', content: 'This is the first post.' })
+  .then(() => updateLastUserActivityTime())
+  .then(() => {
+    console.log("All promises resolved successfully!");
+    console.log("Posts:", posts);
+    console.log("Last activity time:", lastActivityTime);
+    return deleteLastPost();
+  })
+  .then(() => {
+    console.log("Posts after deletion:", posts);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
