3d99a767e828c88e7d9aac24cab040974e8d702a
Saurabh Singh <108921132+codergift@users.noreply.github.com>
2023-04-23 19:37:44 +0530

    promises replaced by async/await

--- MRPD_task13_1.js
+++ MRPD_task13_1.js
@@ -1,29 +1,37 @@
 console.log('person1: shows ticket');
 console.log('person2: shows ticket');
 
-const promiseWifeBringingTicket= new Promise ((resolve, reject)=> {
-  setTimeout(()=>{
-    resolve('ticket');
-  },3000)
-});
+const preMovie = async()=>{
+    const promiseWifeBringingTicket= new Promise ((resolve, reject)=> {
+        setTimeout(()=>resolve('ticket'),3000);
+    });
 
-promiseWifeBringingTicket.then((t)=>{
-  console.log(`person3:shows ${t}`);
-});
+    
+    const getPopcorn=new Promise((resolve, reject)=>resolve(`popcorn`));
+    const addButter=new Promise((resolve, reject)=>resolve(`butter`));
 
-const getPopcorn = promiseWifeBringingTicket.then((t)=>{
-  console.log('wife: i have the ticket');
-  console.log('husband: we should go in');
-  console.log('wife: no i am hungry');
-  return new Promise((resolve, reject)=>resolve(`${t} popcorn`));
-});
-const getButter = getPopcorn.then((t)=>{
-  console.log('husband: i got some popcorn');
-  console.log('husband: we should go in');
-  console.log('wife: I need butter on my popcorn');
-  return new Promise((resolve, reject)=>resolve(`${t} popcorn`));
-});
-getButter.then((t)=> console.log(t))
+    let ticket=await promiseWifeBringingTicket;
+
+    console.log(`wife: i have the ${ticket}`);
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+
+    let popcorn=await getPopcorn;
+
+    console.log(`husband: i got some ${popcorn}`);
+    console.log('husband: we should go in');
+    console.log('wife: I need butter on my popcorn');
+
+    let butter=await addButter;
+    console.log(`wife: I need ${butter} on my popcorn`);
+    console.log('husband: anything else darling?');
+    console.log('wife: lets got we are getting late');
+    console.log('husband: thank you for the remainder *grins*');
+
+    return ticket;
+}
+
+preMovie().then((m)=> console.log(`person3: shows ${m}`));
 
 console.log('person4: shows ticket');
 console.log('person5: shows ticket');
