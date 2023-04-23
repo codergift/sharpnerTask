db90449234027373b816b72984e20081646b36f9
Saurabh Singh <108921132+codergift@users.noreply.github.com>
2023-04-23 19:20:43 +0530

    without async/await and with async/await

--- check.js
+++ check.js
@@ -1,51 +1,29 @@
-let posts = [];
-let lastActivityTime = null;
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
 
-function createPost(postData) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(postData);
-      resolve();
-    }, 1000);
-  });
-}
+const promiseWifeBringingTicket= new Promise ((resolve, reject)=> {
+  setTimeout(()=>{
+    resolve('ticket');
+  },3000)
+});
 
-function updateLastUserActivityTime() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      lastActivityTime = new Date();
-      console.log("User's last activity time updated to:", lastActivityTime);
-      resolve();
-    }, 1000);
-  });
-}
+promiseWifeBringingTicket.then((t)=>{
+  console.log(`person3:shows ${t}`);
+});
 
-function deleteLastPost() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (posts.length === 0) {
-        reject(new Error("No posts to delete"));
-      } else {
-        posts.pop();
-        console.log("Last post deleted");
-        resolve();
-      }
-    }, 1000);
-  });
-}
+const getPopcorn = promiseWifeBringingTicket.then((t)=>{
+  console.log('wife: i have the ticket');
+  console.log('husband: we should go in');
+  console.log('wife: no i am hungry');
+  return new Promise((resolve, reject)=>resolve(`${t} popcorn`));
+});
+const getButter = getPopcorn.then((t)=>{
+  console.log('husband: i got some popcorn');
+  console.log('husband: we should go in');
+  console.log('wife: I need butter on my popcorn');
+  return new Promise((resolve, reject)=>resolve(`${t} popcorn`));
+});
+getButter.then((t)=> console.log(t))
 
-// Usage:
-createPost({ title: 'First post', content: 'This is the first post.' })
-  .then(() => updateLastUserActivityTime())
-  .then(() => {
-    console.log("All promises resolved successfully!");
-    console.log("Posts:", posts);
-    console.log("Last activity time:", lastActivityTime);
-    return deleteLastPost();
-  })
-  .then(() => {
-    console.log("Posts after deletion:", posts);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
