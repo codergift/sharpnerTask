979f15f13dbeaa80658993a36fc926dd7d9890b9
Saurabh Singh <108921132+codergift@users.noreply.github.com>
2023-04-24 07:02:48 +0530

    Promise.all

---
+++ MRPD_task13_3.js
@@ -0,0 +1,29 @@
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
+
+const preMovie = async()=>{
+    const promiseWifeBringingTicket= new Promise ((resolve, reject)=> {
+        setTimeout(()=>resolve('ticket'),3000);
+    });
+
+    
+    const getPopcorn=new Promise((resolve, reject)=>resolve(`popcorn`));
+
+    const getCandy=new Promise((resolve, reject)=>resolve(`candy`));
+
+    const getCoke=new Promise((resolve, reject)=>resolve(`coke`));
+
+
+    let ticket=await promiseWifeBringingTicket;
+
+    let [popcorn, candy, coke]=await Promise.all([getPopcorn, getCandy, getCoke])
+
+    console.log(`${popcorn}, ${candy}, ${coke}`);
+
+    return ticket;
+}
+
+preMovie().then((m)=> console.log(`person3: shows ${m}`));
+
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
